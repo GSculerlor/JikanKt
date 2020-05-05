2d62d20bf5fbe6ef14bdff58d9edadefb0f649d5
Ganendra Afrasya <gsculerlor@hotmail.com>
2020-05-05 16:55:33 +0700

    Update release version

--- src/main/kotlin/moe/ganen/jikankt/JikanClient.kt
+++ src/main/kotlin/moe/ganen/jikankt/JikanClient.kt
@@ -32,6 +32,6 @@ open class JikanClient {
 
     companion object {
         private const val JIKANKT_NAME = "JikanKt"
-        private const val JIKANKT_VERSION = "1.1.2"
+        private const val JIKANKT_VERSION = "1.1.3"
     }
 }
