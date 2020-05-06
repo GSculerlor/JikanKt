e3df1b17a2225624c74b33369ebb917bc65928e9
Ganendra Afrasya <gsculerlor@hotmail.com>
2020-05-07 01:52:42 +0700

    Add producer test case

---
+++ src/test/kotlin/moe/ganen/jikankt/TestCaseProducer.kt
@@ -0,0 +1,53 @@
+package moe.ganen.jikankt
+
+import kotlinx.coroutines.runBlocking
+import moe.ganen.jikankt.connection.RestClient
+import moe.ganen.jikankt.exception.JikanException
+import moe.ganen.jikankt.models.base.types.AnimeSubEntity
+import moe.ganen.jikankt.models.base.types.MalSubEntity
+import moe.ganen.jikankt.models.prod.Producer
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class TestCaseProducer {
+
+    @Test
+    fun `test get Sotsu metadata`() {
+        val expected = Producer(
+            metadata = MalSubEntity(
+                64,
+                "anime",
+                "Sotsu",
+                "https://myanimelist.net/anime/producer/64/Sotsu"
+            )
+        )
+
+        val result = runBlocking { JikanKt.getProducer(64) }
+
+        assertEquals(expected.metadata, result.metadata)
+    }
+
+    @Test
+    fun `test get Studio Pierrot anime`() {
+        val expected = Producer(
+            anime = listOf(AnimeSubEntity(22319, title = "Tokyo Ghoul"), AnimeSubEntity(20, title = "Naruto"))
+        )
+
+        val result = runBlocking { JikanKt.getProducer(1) }
+
+        assertEquals(expected.anime?.get(0)?.title, result.anime?.get(0)?.title)
+        assertEquals(expected.anime?.get(1)?.title, result.anime?.get(1)?.title)
+    }
+
+    @Test(expected = JikanException::class)
+    fun `test get bad ID producer return exception`() {
+        runBlocking { JikanKt.apply { restClient = RestClient(true) }.getProducer(712) }
+    }
+
+    @Test
+    fun `test get bad ID producer`() {
+        val result = runBlocking { JikanKt.apply { restClient = RestClient(false) }.getProducer(712) }
+
+        assert(result.anime.isNullOrEmpty())
+    }
+}
